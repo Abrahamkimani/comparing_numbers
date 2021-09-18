7afe8bd60312f8c6b2ee80488d67a77a5da03fb7
Abrahamkimani <90982487+Abrahamkimani@users.noreply.github.com>
2021-09-18 22:30:18 +0300

    comparing two numbers only
    deleting the third digit

--- main.c
+++ main.c
@@ -8,20 +8,18 @@ int main()
 {
     int num1; /* first number to be read from user */
     int num2;  /* second number to be read from user */
-    int num3; /* this is the third number */ 
+
 
     printf("Enter two digits and I will tell you the relationship\n");
     printf("the relationship they satisfy\n");
 
-    scanf("%d%d%d", &num1, &num2, &num3); /* The two integers to be read*/
+    scanf("%d%d%d", &num1, &num2); /* The two integers to be read*/
 
     if (num1 == num2) {
         printf( "%d is equal to %d\n", num1, num2);
     } /* End if */
 
-    if (num1 == num3) {
-        printf( "%d is equal to %d\n", num1, num3);
-    } /* End if */
+
 
     if ( num1!=num2 ) {
         printf( "%d is not equal to %d\n", num1, num2);
